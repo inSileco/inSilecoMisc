2da733f0481b3df3dddad8f1cdbb6c26354b5913
Kevin Cazelles <kcazelle@uoguelph.ca>
2017-11-03 17:59:16 -0400

    add a test of con parameter in substrBib

--- tests/testthat/test-findRef.R
+++ tests/testthat/test-findRef.R
@@ -4,12 +4,15 @@ context("Test substrBib")
 file.name <- system.file('Bib', 'RJC.bib', package='RefManageR')
 bib <- RefManageR::ReadBib(file.name)
 tfile <- tempfile(fileext='.md')
-##
+tfile2 <- tempfile(fileext='.tex')
+####
 txt <- 'First \\cite{kim1995estimating}, second \\Citep{fu2006statistical}'
 txtmd <- 'First @Pimm2000, second @May1972, third [@May1972]'
 cat(txtmd, sep='\n', file=tfile)
-##
+cat(txt, sep='\n', file=tfile2)
+####
 out <- substrBib(bib, txt)
+out2 <- substrBib(bib, con = tfile2)
 res <- findRef(text='First \\cite{Pimm2000}, second \\Citep{May1972}')
 res2 <- findRef(text=txtmd, markdown=TRUE)
 res3 <- findRef(con=tfile, markdown=TRUE)
@@ -21,6 +24,8 @@ test_that("substrBib", {
   expect_equal(length(out), 2)
   expect_equal(gsub(out[1L], pattern='\\D', replace=""), "12006212630")
   expect_equal(gsub(out[2L], pattern='\\D', replace=""), "1319951437144614")
+  expect_equal(gsub(out2[1L], pattern='\\D', replace=""), "12006212630")
+  expect_equal(gsub(out2[2L], pattern='\\D', replace=""), "1319951437144614")
 })
 
 
