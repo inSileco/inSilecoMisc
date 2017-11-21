ee75cc19e1cef508789617d898725b2b700c41c6
Kevin Cazelles <kcazelle@uoguelph.ca>
2017-11-21 14:43:39 -0500

    add findReplace and grepReplace

---
+++ R/findReplace.R
@@ -0,0 +1,40 @@
+#' @title Find and replace
+#'
+#' @description
+#' Functions to locate certain pattern and replace them.
+#'
+#' @author
+#' Kevin Cazelles
+#'
+#' @param vec a vector where to search for patterns.
+#' @param vec_pat vector providing the patterns to be searched for.
+#' @param y values to replace matching values.
+#'
+#' @importFrom magrittr %>%
+#' @export
+#'
+#' @details
+#' These functions may be useful in case where many different patterns must be
+#' replaced by the same value. Note that there are many other way to proceed.
+#'
+#' @example
+#' vec <- c('fish', 'crab', 'crabs', 'bug', 'crab')
+#' findReplace(vec, 'crab', 'fish')
+#' findReplace(vec, c('crab', 'crabs'), 'fish')
+#' grepReplace(vec, 'crab[s]?', 'fish')
+#' @describeIn findReplace Return a vector including the desire modifications.
+
+findReplace <- function(vec, vec_pat, y) {
+    vec[vec %in% vec_pat] <- y
+    return(vec)
+}
+
+
+#' @describeIn findReplace Return a vector including the desire modifications.
+#' @export
+grepReplace <- function(vec, vec_pat, y) {
+    res <- lapply(vec_pat, function(x) grepl(vec, pattern = x))
+    res2 <- do.call(cbind, res) %>% apply(1L, sum)
+    vec[res2 > 0] <- y
+    return(vec)
+}
---
+++ tests/testthat/test-findReplace.R
@@ -0,0 +1,15 @@
+library(letiRmisc)
+context("findReplace")
+
+vec <- c("fish", "crab", "crabs", "bug", "crab")
+res1 <- findReplace(vec, "crab", "fish")
+res2 <- findReplace(vec, c("crab", "crabs"), "fish")
+res3 <- grepReplace(vec, "crab[s]?", "fish")
+
+
+
+test_that("findReplace", {
+  expect_true(all(res1 == c("fish", "fish", "crabs", "bug", "fish")))
+  expect_true(all(res2 == c("fish", "fish", "fish", "bug", "fish")))
+  expect_true(all(res3 == c("fish", "fish", "fish", "bug", "fish")))
+})
