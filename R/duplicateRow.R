21532065ea46cf1b79b4b44ff3e7065f7d4cbc15
Kevin Cazelles <kcazelle@uoguelph.ca>
2018-06-13 13:13:52 -0400

    tests duplicateCol using columns' names :shield:

--- R/duplicateRow.R
+++ R/duplicateRow.R
@@ -1,15 +1,16 @@
-#' Duplicate element of a dataframe.
+#' Duplicates elements of a dataframe.
 #'
 #' Duplicates rows and colmns of a given a dataframe.
 #'
 #' @author
 #' Kevin Cazelles
+#'
 #' @param x a data.frame.
 #' @param id.el identity of the elements to be duplicated.
 #' @param times number of times elements are duplicated. Could be a vector of the same length as id.el.
 #' @param append A logical. If \code{TRUE}, duplicated elements will be appended to the dataframe otherwise duplicated elements remain next to their parent. Non-existing columns cannot be duplicated while non-existing rows can and produce \code{NA}.
-#' @importFrom magrittr %>%
-#' @importFrom magrittr %<>%
+#'
+#' @importFrom magrittr %>% %<>%
 #' @export
 #' @examples
 #' data(iris, package='datasets')
@@ -17,23 +18,26 @@
 #' iris3 <- duplicateCol(iris, id.el=c('Petal.Length', 'Petal.Width'), times=c(1,2), append=TRUE)
 
 
-#' @describeIn duplicateRow A dataframe with duplicated rows.
+#' @describeIn duplicateRow returns a dataframe with duplicated rows.
 duplicateRow <- function(x, id.el = 1, times = 1, append = FALSE) {
     pos <- rep(id.el, times) %>% sort
     if (class(pos) == "character") 
         ord <- c(rownames(x), pos) else ord <- c(1:nrow(x), pos)
     if (!append) 
         ord %<>% sort
-    return(x[ord, ])
+    
+    x[ord, ]
 }
 
-#' @describeIn duplicateRow A dataframe with duplicated columns.
+#' @describeIn duplicateRow returns a dataframe with duplicated columns.
 #' @export
+
 duplicateCol <- function(x, id.el = 1, times = 1, append = FALSE) {
     pos <- rep(id.el, times) %>% sort
     if (class(pos) == "character") 
         ord <- c(colnames(x), pos) else ord <- c(1:ncol(x), pos)
     if (!append) 
         ord %<>% sort
-    return(x[, ord])
+    
+    x[, ord]
 }
