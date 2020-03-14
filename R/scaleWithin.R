3e5c3143ef6893c0ef4c00f1da75ba39beea1e90
Kevin Cazelles <kcazelle@uoguelph.ca>
2020-03-14 13:32:00 -0400

    :pencil: review scaleWithin() doc

--- R/scaleWithin.R
+++ R/scaleWithin.R
@@ -8,7 +8,16 @@
 #' @param mn minimum value (`mn` and lower values are set to 1).
 #' @param mx maximum value (`mx` and higher values are set to `nval`).
 #'
-#'
+#' @details
+#' This function was originally created to ease the creation of custom color
+#' scales. In such context, it is common practice to define a minium and a
+#' maximum for the set of values to be displayed values as well as a number of
+#' color to be used in the color scale (`nval`). This is exactly what
+#' `scaleWithin()` does: minimum and maximum are by default the ones of the set
+#' of values `x` but can also be set using `mn` and `mx` in which case values
+#' below `mn` will be set to `mn` (and thus to 1 in the output) and values
+#' above `mx` will be set to `mx` (and thus to 1 in the output). Finally, the
+#' number of colors to be used used in is given by `nval`.
 #'
 #' @return
 #' A numeric vector
@@ -18,7 +27,6 @@
 #' x <- stats::rpois(20, 20)
 #' scaleWithin(x, 20, 10, 30)
 #' scaleWithin(matrix(x, 10, 2), mn = 20, mx = 30)
-
 
 scaleWithin <- function(x, nval = 100, mn = min(x), mx = max(x)) {
     stopifnot(mn < mx)
