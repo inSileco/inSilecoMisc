4e430fd7b0b6e80a3c8f21a9db49ab395f5ccede
Kevin Cazelles <kcazelle@uoguelph.ca>
2020-03-22 14:46:42 -0400

    :pencil: use unicde in tests for msgFUNs()

--- tests/testthat/test-msgFUNs.R
+++ tests/testthat/test-msgFUNs.R
@@ -7,9 +7,10 @@ msg4 <- msgWarning("be careful")
 msg5 <- msgInfo("computing", "again")
 
 test_that("msgFUNs", {
-  expect_equal(msg1, "ℹ computing")
-  expect_equal(msg2, "✔ done")
-  expect_equal(msg3, "✖ you got it wrong")
-  expect_equal(msg4, "⚠ be careful")
-  expect_equal(msg5, "ℹ computing again")
+  expect_equal(msg1, "\u2139 computing")
+  expect_equal(msg2, "\u2714 done")
+  expect_equal(msg3, "\u2716 you got it wrong")
+  expect_equal(msg4, "\u26a0 be careful")
+  expect_equal(msg5, "\u2139 computing again")
 })
+
