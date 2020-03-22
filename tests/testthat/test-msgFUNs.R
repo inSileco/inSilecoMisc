85844168eb1cf758f6d1288562e9592523d53b96
Kevin Cazelles <kcazelle@uoguelph.ca>
2020-03-22 14:59:14 -0400

    :pencil: test message with stringi

--- tests/testthat/test-msgFUNs.R
+++ tests/testthat/test-msgFUNs.R
@@ -7,10 +7,14 @@ msg4 <- msgWarning("be careful")
 msg5 <- msgInfo("computing", "again")
 
 test_that("msgFUNs", {
-  expect_equal(msg1, "\u2139 computing")
-  expect_equal(msg2, "\u2714 done")
-  expect_equal(msg3, "\u2716 you got it wrong")
-  expect_equal(msg4, "\u26a0 be careful")
-  expect_equal(msg5, "\u2139 computing again")
+  expect_equal(stringi::stri_enc_toascii(msg1),
+    stringi::stri_enc_toascii("ℹ computing"))
+  expect_equal(stringi::stri_enc_toascii(msg2),
+    stringi::stri_enc_toascii("✔ done"))
+  expect_equal(stringi::stri_enc_toascii(msg3),
+    stringi::stri_enc_toascii("✖ you got it wrong"))
+  expect_equal(stringi::stri_enc_toascii(msg4),
+    stringi::stri_enc_toascii("⚠ be careful"))
+  expect_equal(stringi::stri_enc_toascii(msg5),
+    stringi::stri_enc_toascii("ℹ computing again"))
 })
-
